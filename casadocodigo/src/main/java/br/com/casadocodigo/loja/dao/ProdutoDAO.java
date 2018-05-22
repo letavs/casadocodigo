1cd6f13db947e0e52af972e5adcd7cfb47afb91d
Leandro Tavares <letavs@gmail.com>
2018-05-21 22:15:43 -0300

    Update ProdutoDAO
    Add comentario

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -7,9 +7,9 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import br.com.casadocodigo.loja.models.Produto;
 
+// Projeto de Banco de dados
 @Repository
 @Transactional
 public class ProdutoDAO {
